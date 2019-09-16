c16e2809f0a090402288e54975ba544cf5ba937b
RedowanRatul <ratul06oct@gmail.com>
2019-09-16 14:32:30 +1000

    Second commit

--- 90015_GitProject/src/MainClass.java
+++ 90015_GitProject/src/MainClass.java
@@ -3,7 +3,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!!!, I am Here");
+		
+		for(int i=0; i<10; i++)
+			System.out.println("Hello World!!!!!, I am Here");
 
 	}
 
